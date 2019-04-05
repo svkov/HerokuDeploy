d3b8130c2d444e39b998a7709a718d24ba98a0ea
Святослав <kovalev.svyatoslav42@gmail.com>
2019-04-05 20:40:06 +0300

    init commit

---
+++ hello/tests.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+
+from hello.models import Something
+# Create your tests here.
+
+
+class SomeTestCase(TestCase):
+    def setUp(self):
+        Something.objects.create(name='abc')
+        Something.objects.create(name='sjdfsdk')
+
+    def testName(self):
+        self.assertEqual(Something.objects.get(id=1).name, 'abc')
+        self.assertEqual(Something.objects.get(id=2).name, 'sjdfsdk')
+
+    def testCreation(self):
+        name = 'abcdefghklnopqrstuvwxyz'
+        Something.objects.create(name=name)
+        self.assertEqual(Something.objects.get(id=3).name, name)
+
