3bea6c3f41d08e3e8cf0d1987c06a79b8e2a00be
Святослав <kovalev.svyatoslav42@gmail.com>
2019-04-05 22:07:11 +0300

    static work pls

--- HerokuDeploy/settings.py
+++ HerokuDeploy/settings.py
@@ -119,6 +119,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATIC_ROOT = '/var/www/heroku-test/static'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
