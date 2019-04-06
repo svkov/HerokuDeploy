532752fdb365dc2997d66e58344f0567db4f25a7
Святослав <kovalev.svyatoslav42@gmail.com>
2019-04-06 22:58:36 +0300

    add css

--- HerokuDeploy/settings.py
+++ HerokuDeploy/settings.py
@@ -116,26 +116,16 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-
-# Try to import django-heroku depending on Travis or Heroku
 try:
-    # Configure Django App for Heroku.
     import django_heroku
     django_heroku.settings(locals())
 except ImportError:
