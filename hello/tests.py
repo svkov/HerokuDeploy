1df7df51f315170cdaea31f3b0de0d7d79a14ec2
Святослав <kovalev.svyatoslav42@gmail.com>
2019-04-06 16:20:57 +0300

    add hello world test

--- hello/tests.py
+++ hello/tests.py
@@ -1,21 +1,32 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 from hello.models import Something
+
+
 # Create your tests here.
 
 
 class SomeTestCase(TestCase):
     def setUp(self):
-        Something.objects.create(name='abc')
-        Something.objects.create(name='sjdfsdk')
-"""
+        Something.objects.create(text='abc')
+        Something.objects.create(text='sjdfsdk')
+
     def testName(self):
-        self.assertEqual(Something.objects.get(id=1).name, 'abc')
-        self.assertEqual(Something.objects.get(id=2).name, 'sjdfsdk')
+        self.assertEqual(Something.objects.get(id=1).text, 'abc')
+        self.assertEqual(Something.objects.get(id=2).text, 'sjdfsdk')
 
     def testCreation(self):
         name = 'abcdefghklnopqrstuvwxyz'
-        Something.objects.create(name=name)
-        self.assertEqual(Something.objects.get(id=3).name, name)
+        Something.objects.create(text=name)
+        self.assertEqual(Something.objects.get(id=3).text, name)
 
-"""
+
+class TestViews(TestCase):
+    client = None
+
+    def setUp(self):
+        self.client = Client()
+
+    def testHelloWorld(self):
+        ans = self.client.get('/')
+        self.assertEqual(ans.status_code, 200)
