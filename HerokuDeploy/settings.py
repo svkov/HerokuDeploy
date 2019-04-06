20657a3504f5a6db8128cb56dac8fdb23a7827c5
Святослав <kovalev.svyatoslav42@gmail.com>
2019-04-06 23:38:18 +0300

    debug = true

--- HerokuDeploy/settings.py
+++ HerokuDeploy/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&63pl7diylkxyl33q$)all=1h((kehnz9pbffm!4n0t+#n$mbb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
